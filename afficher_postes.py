96e7325663af77d084bd7a076bb97b789a39746a
Amine El Khanchaf <amkh1772@gmail.com>
2026-09-03 17:24:00 +0200

    final commit

--- afficher_postes.py
+++ afficher_postes.py
@@ -1,14 +1,14 @@
 import pypowsybl as pp
 
 NETWORK_FILE = "data/initial.xiidm"
-VOLTAGE_LEVEL = "CRENEP3"
+VOLTAGE_LEVEL = ".A.ZA 6"
 
 network = pp.network.load(NETWORK_FILE)
 
 network.write_single_line_diagram_svg(
     VOLTAGE_LEVEL,
-    "CRENEP3_single_line.svg",
+    ".A.ZA 6_single_line.svg",
 )
 
-print("Schéma généré : CRENEP3_single_line.svg")
+print("Schéma généré : .A.ZA 6_single_line.svg")
 
